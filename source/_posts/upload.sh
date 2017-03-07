af298edaebc4df150d4e6b923a851e1f61e168ba
yiddu <yiddu@thoughtworks.com>
2017-03-07 09:45:13 +0800

    latest update for last blog

---
+++ source/_posts/upload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DATA=`date`
+
+if [ $# -eq 1 ]; then
+    MESS=$1
+else
+    MESS=''
+fi
+
+git add .
+git commit -m "$DATA---$MESS"
+git push origin master
+
+cd ../.. 
+hexo g 2>>/dev/null 
+hexo d 2>>/dev/null 
